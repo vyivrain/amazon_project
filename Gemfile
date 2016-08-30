0a1ceacad2d51cad9d1f1171fd4d842f7975af6e
vyivrain <stranger9983@gmail.com>
2016-08-30 18:17:20 +0300

    New stuff

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.6'
+gem 'pg', '~> 0.15'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'figaro'
+gem 'puma'
+gem 'bootstrap-sass'
+gem 'simple_form'
+gem 'slim-rails'
+
+group :development, :test do
+  gem 'byebug'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+  gem 'capistrano'
+  gem 'capistrano3-puma'
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-rvm'
+end
+
---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'amazon_project'
+set :repo_url, 'git@example.com:me/my_repo.git'
+set :branch, :master
+set :deploy_to, '/home/deploy/amazon_project'
+set :pty, true
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 2
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.0p0' # Edit this if you are using MRI Ruby
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
