8d3d9020f5cbd55d2f66265c54d9afa54c112f01
vyivrain <stranger9983@gmail.com>
2016-11-17 11:51:23 +0200

    Remove idea, add things to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,16 +25,3 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
