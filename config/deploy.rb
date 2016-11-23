4fc25fb151303dd4d471bf754deeba2b4b089b01
vyivrain <stranger9983@gmail.com>
2016-11-23 12:32:13 +0200

    Another configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 2
-set :rvm_type, :user
+set :rvm_type, :system
 set :rvm_ruby_version, '2.3.1'
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :user, 'ubuntu'
 server 'ubuntu@ec2-35-164-181-183.us-west-2.compute.amazonaws.com', roles: [:app, :web, :db], primary: true
 set :stage, 'production'
-ssh_options = { keys: ['~/.ssh/amazon_rsa.pem'], forward_agent: false, user: 'ubuntu' }
+set :ssh_options, { keys: ['~/.ssh/amazon_rsa.pem'], forward_agent: true, user: 'ubuntu' }
