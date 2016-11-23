0a3671a3f66b79adea7f3c9483be8e6aa2e52905
vyivrain <stranger9983@gmail.com>
2016-11-23 12:08:21 +0200

    Some fixes to config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@
 set :user, 'ubuntu'
 server 'ubuntu@ec2-35-164-181-183.us-west-2.compute.amazonaws.com', roles: [:app, :web, :db], primary: true
 set :stage, 'production'
-ssh_options = { keys: ['~/.ssh/my_amazon.pem'], forward_agent: true, user: 'ubuntu' }
+ssh_options = { keys: ['~/.ssh/amazon_rsa.pem'], forward_agent: true, user: 'ubuntu' }
 
 # Defines a role with one or multiple servers. The primary server in each
 # group is considered to be the first unless any  hosts have the primary
